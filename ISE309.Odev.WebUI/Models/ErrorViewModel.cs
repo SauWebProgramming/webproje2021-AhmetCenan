287aa29292e8eec7de686842b5da5f1567289039
cenano54 <akdogan54ahmet@gmail.com>
2021-12-15 22:29:10 +0300

    Oluşturuldu

---
+++ ISE309.Odev.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ISE309.Odev.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
